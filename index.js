678f1aea2368c919c8d0e0bc7843cb80b11198e8
em <em@emp.(none)>
2013-01-24 12:07:36 +0100

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+	
+	
+	
+	module.exports = function( classDefinition ){
+
+	};
+
+
+
+
+
+
+	module.exports = Class( {
+		inherits: Events
+
+		, hui: {
+			a: 2
+		}
+
+		, init: function( options ){
+			this.parent.init();
+		}
+	} );
