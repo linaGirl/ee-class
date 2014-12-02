3aff812539235d19975d7cdebc4e6a3dc6f5ffe7
Christopher Maples <cmaples@singularity-interactive.com>
2014-12-01 21:59:49 -0700

    Adds EventEmitter class from eventEmitter/ee-event-emitter in this package.  Adds grunt and minify task.

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+                sourceMap: true
+            },
+            Class: {
+                src: 'lib/Class.js',
+                dest: 'dist/Class.min.js'
+            },
+            EventEmitter: {
+                src: 'lib/EventEmitter.js',
+                dest: 'dist/EventEmitter.min.js'
+            }
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Default task(s).
+    grunt.registerTask('default', ['uglify']);
+
+};
